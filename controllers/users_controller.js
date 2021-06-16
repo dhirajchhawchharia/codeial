38b98b096c7e2f8910e10de62625b69dc21a313e
Dhiraj Chhawchharia <dhirajchhawchharia27@gmail.com>
2021-06-17 02:56:07 +0530

    Add users, about routes and create thier controllers

---
+++ controllers/users_controller.js
@@ -0,0 +1,15 @@
+module.exports.profile = function(req, res){
+    return res.end('<h1>User Profile</h1>');
+};
+
+module.exports.likes = function(req, res){
+    return res.end('<h1>Likes Likes</h1>');
+};
+
+module.exports.default = function(req, res){
+    return res.end('<h1>Default</h1>');
+}
+
+module.exports.posts = function(req, res){
+    return res.end('<h1>Posts</h1>');
+};
